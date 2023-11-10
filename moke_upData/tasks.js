109a7e0a79d052270a68533475594100d967691d
22guerrabruno <22guerrabruno@gmail.com>
2023-11-10 21:30:19 +0100

    segundo commit

---
+++ moke_upData/tasks.js
@@ -0,0 +1,27 @@
+const tasks = [
+  {
+    title: 'Comprar leche',
+    description: 'Ir al super y comprar leche',
+    status: 'pending',
+    dateStart: '2023-11-10',
+    dateEnd: '2023-11-10',
+    id: 'string',
+    user: 'string',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+  {
+    title: 'Comprar leche',
+    description: 'Ir al super y comprar leche',
+    status: 'pending',
+    dateStart: '2023-11-10',
+    dateEnd: '2023-11-10',
+    id: 'string2',
+    user: 'string',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+];
+module.exports = tasks;
