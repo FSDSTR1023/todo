71f3ff3e2a2a2e12a664ef87a87416ea7a938606
albertsube <albert.sune.belmonte@gmail.com>
2023-11-11 18:54:32 +0100

    ft: Endpoints Tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,36 +1,83 @@
 const express = require('express')
 const router = express.Router()
 
-const tasks = require('../mockup_data/tasks')
+let tasks = require('../mockup_data/tasks')
 
+// GET/tasks : Get a incomplete task list
 router.get('/', (req, res) => {
-    console.log('query params', req.query)
-    res.json(tasks)
-  })
+    res.json(tasks.filter( task => task.status !== 'COMPLETED' && task.deletedAt === null ))
+})
   
-  router.get('/:id', (req,res) => {
-    console.log('unique task', req.params, req.query)
-    res.json(tasks[0])
+// GET/tasks/id: Get a detail of a task
+router.get('/:id', (req,res) => {
+  const { id } = req.params
+  res.json(tasks.find( task => task.id === id ))
+})
+
+// PUT/tasks/id: Update a task
+router.put('/:id', (req,res) => {
+  
+  const { id } = req.params
+  const oldTask = tasks.find( task => task.id === id )
+  const newTask = { ...oldTask, ...req.body }
+
+  tasks = tasks.map( task => task.id === id ? 
+    newTask
+    :
+    task
+  )
+
+  res.json({
+      msg: 'task updated successfully',
+      oldTask,
+      newTask,
+  })
+})
+
+// DELETE/task/id: Remove a task
+router.delete('/:id', (req,res) => {
+
+  const { id } = req.params
+  const currentDate = require('../helpers/date')
+  console.log(currentDate())
+
+  tasks = tasks.map( task => task.id === id ?
+    { ...task, deletedAt: currentDate() }
+    :
+    task  
+  )
+
+  res.json({
+      msg: "Task " + id + " has been deleted"
+  })
+})
+
+// POST/task/: Create a new task
+router.post('/', (req,res) => {
+  
+  tasks = [ ...tasks, req.body ]
+
+  res.json({
+      msg: 'Task created successfully'
+  })
+})
+
+// PATCH/task/id: Mark as completed
+router.patch('/:id', (req,res) => {
+
+  const { id } = req.params
+
+  tasks = tasks.map( task => task.id === id ?
+      { ...task, status: "COMPLETED" }
+      :
+      task
+  )
+
+  res.json({
+    msg: `Task ${id} completed successfully`
   })
 
-  router.post('/', (req,res) => {
-    console.log('create task', req.body)
-    res.json({
-        msg: 'task created successfully'
-    })
-  })
+})
 
-  router.put('/:id', (req,res) => {
-    console.log('update task', req.params, req.body)
-    res.json({
-        msg: 'task updated successfully'
-    })
-  })
 
-  router.delete('/:id', (req,res) => {
-    res.json({
-        msg: "Task " + req.params.id + " has been deleted"
-    })
-  })
-
-  module.exports = router
+module.exports = router
