bfae17b67c033461d99401f47835595fc78087dd
Enrique Garcia <enrique@amigosdelcara.com>
2023-11-13 16:41:11 +0100

    Commit inserting code of login

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,19 @@ const router = express.Router()
 
 router.get('/',(req,res)=>{
     res.json({
-        "user":"Enrique García"
+        firstname:"Enrique García",
+        lastname: "García",
+        email: "test@example.com"
     })
+})
+
+router.post('/login',(req,res)=>{
+    const statusCode = parseInt(res.statusCode);
+    switch (statusCode){
+        case 200: res.json({msg: "Login succesful"})
+        case 403: res.json({msg: "Forbidden"})
+        case 404: res.json({msg: "User not found"})
+    }
 })
 
 module.exports = router
