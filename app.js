b03d3867abf30b4b43e69db74eed57ae35b98513
Carmen Plaza <carmenplazaromero@gmail.com>
2023-11-20 18:18:23 +0100

    secon commit

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
-//ÉSTE CÓDIGO LO COGEMOS Y PEGAMOS DE https://expressjs.com/en/starter/hello-world.html
-const express = require('express')  //auqi decimos queremos el paquete express
-const app = express()           // aqui decimos lo vamos a guardar y ejecutar en una aplicacion app
-const port = 3000              // lo vamos a poner en este puerto
+const express = require('express')
+const app = express()            
+const port = 3000              
 
-app.get('/', (req, res) => {        // mi app va a responder a las peticiones .get con la direccion: ('/', que va aqui) 
-                                    //y esto va a tener una request req (solicitud) y una respuesta res. 
-                                    //la solicitud vendrá del cliente fronten y la respuesta es el mensaje hello wordld
-  
-    res.send('Hello World!')
+app.use(express.json());
 
-})
-app.get('/patata', (req, res) => { 
-    res.send('he creado patata???... <br> bueno, buenoo, bueno...<br> menuda patata instalar nodemon <br> HACERLO DESDE TERMINAL BASH')     
+const tasks = require('./routes/tasks');
+app.use('/tasks', tasks);
 
-})
-app.get('/hola', (req, res) => { 
-    res.status(403)    
-    res.json({
-        msg: "no tienes acceso",
-     })
-})
-app.get('/tasks', (req, res) => { 
-        res.json([{
-            title:"preparar maletas",
-            
+const users = require('./routes/users');
+app.use('/users', users);
 
-        }
 
-        ])
-       
-     
-})
 
-app.post('/', (req, res) => { 
-    res.send("hola post")
-})
-
-app.listen(port, () => {            //nuestra aplicaicón va a escuchar en un puerto (que es el que tenemos arriba)
-  console.log(`Example app listening on port ${port}`)  //aqui nos va a decir en en eel console log en que puerto nos está escuchando
-})
+app.listen(port, () => {            
+  console.log(`Example app listening on port ${port}`) 
+});
---
+++ moke_upData/tasks.js
@@ -0,0 +1,33 @@
+const tasks = (
+    {
+
+       title: "Preparar las maletas",
+       description: "El viaje a Marte va a durar unos cuantos meses, preparar ropa de verano e invierno",
+       status: "PENDIN", //PENDING IN PROGRESS, COMPLETED
+       datestart: "2023-11-08 22:00:00",
+       dateen: "2023-12-08 22:00:00",
+       id: "7876454412l2",
+       user: "jgalobart",
+       createdAt: "2023-11-10 20:03:00",
+       modifiedAt: "2023-11-08 22:05:00",
+       deteteAt: null
+
+   
+
+   },
+   {
+       title: "Preparar las maletas",
+       description: "El viaje a Marte va a durar unos cuantos meses, preparar ropa de verano e invierno",
+       status: "PENDIN", //PENDING IN PROGRESS, COMPLETED
+       datestart: "2023-11-08 22:00:00",
+       dateen: "2023-12-08 22:00:00",
+       id: "odjflk45454542",
+       user: "juanita",
+       createdAt: new Date(),
+       modifiedAt: new Date(),
+       deteteAt: null
+
+
+   }
+
+    );
