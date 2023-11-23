8a386e47b9f133daa51a9a1e17898b733b6d72ed
Miki <miquelxubet@gmail.com>
2023-11-23 23:42:27 +0100

    first commit

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
-  
+
+const statuses = {
+  PENDING: "PENDING",
+  IN_PROGRESS: "IN PROGRESS",
+  COMPLETED: "COMPLETED",
+};
+
 const tasks =  [
     {
       "title":"Preparar maletas",
       "description": "El viaje a marte va a durar unos cuantos meses, preparar ropa de verano e invierno",
-      "status": "PENDING", //PENDING, IN PROGRESS, COMPLETED
+      "status": statuses.PENDING, //PENDING, IN PROGRESS, COMPLETED
       "datestart": "2023-11-08 22:00:00",
       "dateend": "2023-12-08 22:00:00",
       "id": "aaa6465425",
@@ -17,22 +23,34 @@ const tasks =  [
     {
       "title":"Preparar las maletas",
       "description": "El viaje a marte va a durar unos cuantos meses, preparar ropa de verano e invierno",
-      "status": "PENDING", //PENDING, IN PROGRESS, COMPLETED
+      "status": statuses.PENDING, //PENDING, IN PROGRESS, COMPLETED
       "datestart": "2023-11-08 22:00:00",
       "dateend": "2023-12-08 22:00:00",
-      "id": "aaa6465425",
+      "id": "aaafd6239425",
       "user": "mxubet",
       "createdAt": "2023-11-10 20:03:00",
       "modifiedAt":"2023-11-08 22:05:00",
       "deletedAt": null
     },
     {
-      "title":"Preparar las maletas",
-      "description": "El viaje a marte va a durar unos cuantos meses, preparar ropa de verano e invierno",
-      "status": "PENDING", //PENDING, IN PROGRESS, COMPLETED
+      "title":"Organizar las rutas",
+      "description": "Establecer las paradas y los sitios a visitar cada día durante el viaje.",
+      "status": statuses.IN_PROGRESS, //PENDING, IN PROGRESS, COMPLETED
       "datestart": "2023-11-08 22:00:00",
       "dateend": "2023-12-08 22:00:00",
-      "id": "aaa6465425",
+      "id": "aaafd6451425",
+      "user": "mxubet",
+      "createdAt": "2023-11-10 20:03:00",
+      "modifiedAt":"2023-11-08 22:05:00",
+      "deletedAt": null
+    },
+    {
+      "title":"Preparar la comida",
+      "description": "Preparar despensa para varios días de comida.",
+      "status": statuses.COMPLETED, //PENDING, IN PROGRESS, COMPLETED
+      "datestart": "2023-11-08 22:00:00",
+      "dateend": "2023-12-08 22:00:00",
+      "id": "aagda6446765425",
       "user": "mxubet",
       "createdAt": "2023-11-10 20:03:00",
       "modifiedAt":"2023-11-08 22:05:00",
@@ -41,20 +59,33 @@ const tasks =  [
 ]   
    
   router.get('/', (req, res) => {
-    res.json(tasks)
-  })
+    const onlyPendingTasks = tasks.filter((task) => {
+    return task.status !== statuses.COMPLETED;
+  });
+    res.json(onlyPendingTasks);
+  });
 
-  router.get('/:id', (req,res) => {
-    res.json(tasks[0])
-  })
+  router.get('/:id', (req, res) => {
+    const taskId = req.params.id;
+
+    const taskById = tasks.find((task) => {
+      return task.id.toString() === taskId;
+    });
+
+    if (!taskById) {
+      return res.status(404).send("Task not found");
+    }
+
+    res.json(taskById);
+  });
 
   router.post('/', (req, res) => {
-    console.log("task created", req.query,req.params,req.body)
+    console.log("create task", req.query,req.params,req.body)
     res.json({msg: 'task created succesfully'})
   })
 
   router.put('/:id', (req, res) => {
-    console.log("task updated", req.query,req.params,req.body)
+    console.log("update task", req.query,req.params,req.body)
     res.json({msg: 'task updated succesfully'})
   })
 
