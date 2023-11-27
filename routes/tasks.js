01d0f518eee00f03a19839863d7cc3e9834c4949
Enrique Garcia <enrique@amigosdelcara.com>
2023-11-27 18:38:56 +0100

    task solution error

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,11 +3,11 @@ const router = express.Router()
 
 const taskController = require('../controllers/tasks.controller')
 
-router.post('/tasks',taskController.createTask )
-router.put('/tasks/:id',taskController.updateTask )
-router.patch('/tasks/:id', taskController.patchTaskById )
-router.get('/tasks/:id', taskController.getTaskById )
-router.get('/tasks', taskController.getTasks )
-router.delete('/tasks/:id', taskController.deleteTaskById )
+router.post('/',taskController.createTask )
+router.put('/:id',taskController.updateTask )
+router.patch('/:id', taskController.patchTaskById )
+router.get('/:id', taskController.getTaskById )
+router.get('/', taskController.getTasks )
+router.delete('/:id', taskController.deleteTaskById )
 
 module.exports = router
