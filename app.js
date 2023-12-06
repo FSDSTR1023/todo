6d4815bdc484f94699c4991f5b359f3a90c3e1fc
Pablo Labarta <plabartalarrauri@gmail.com>
2023-12-06 10:47:21 +0000

    Estableciendo base backend

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+require('dotenv').config(); //meteremos las variables de enterno
+
+const mongoose = require("mongoose");
+const mongoDB = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_SERVER+"/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+main().catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    console.log(process.env.DB_USER, '<--- ¿BBDD conectada? de app.js');
+    res.send('BACKEND WORKING 😀')
+})
+
+app.listen(port, ()=> {
+    console.log('Server running on port: ', port);
+})
