b458fbde66a1b4b7e788eca8ebdf015ed4f20cbe
Miki <miquelxubet@gmail.com>
2024-01-05 03:18:17 +0100

    conexió amb mongodb commit

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+const cors = require("cors")
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials: true
+}))
+require('dotenv').config();
 
-const tasks = require('./routes/tasks')
+const mongoose = require("mongoose");
+const mongoDB = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_SERVER+"/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+main().catch(err => console.log(err));
+
+var tasks = require('./routes/tasks')
 app.use('/tasks', tasks)
 
 const users = require('./routes/users')
